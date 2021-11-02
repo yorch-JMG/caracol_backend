c6488ecf8be711b03dabc8eaf137ba9e4cd93228
Jorge <jorgeMG@jorgemdev.com>
2021-11-02 14:16:40 -0700

    Passwords hash implemented

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import faker from 'faker';
 import {connection} from "../connection";
+import bcryptjs from 'bcryptjs';
 import mysql from 'mysql';
 export const router = Router();
 
@@ -15,13 +15,15 @@ router.get('/api/users', (req, res) => {
 router.get('/api/users/create', async (req, res) => {
   const nombre = req.body.nombre;
   const contrasena = req.body.contrasena;
+
+  const encryptedPwd = bcryptjs.hashSync(contrasena, 8);
   const puesto = req.body.puesto;
   const departamento = req.body.departamento;
   const correoElectronico = req.body.correoElectronico;
   const telefono = req.body.telefono;
   
   const createUser = "CALL createUser(?,?,?,?,?,?)";
-  const query = mysql.format(createUser, [nombre, contrasena, puesto, departamento, correoElectronico, telefono]); 
+  const query = mysql.format(createUser, [nombre, encryptedPwd, puesto, departamento, correoElectronico, telefono]); 
   
     connection.query( query,
                      (err, result) => {
